865fcb7f418e4befc8787ad12d8e852778c2cf41
HoriSho <shohori13@gmail.com>
2019-12-11 17:20:44 +0900

    QR scan setting

--- rakuXsta.Android/MainActivity.cs
+++ rakuXsta.Android/MainActivity.cs
@@ -21,12 +21,24 @@ namespace rakuXsta.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //zxing
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+
             LoadApplication(new App());
         }
+        /*
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        */
 
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
--- rakuXsta/Pages/QRScanPage.xaml.cs
+++ rakuXsta/Pages/QRScanPage.xaml.cs
@@ -5,11 +5,34 @@ using Xamarin.Forms;
 
 namespace rakuXsta.Pages
 {
-    public partial class CameraPage : ContentPage
+    public partial class QRScanPage : ContentPage
     {
-        public CameraPage()
+        public QRScanPage()
         {
             InitializeComponent();
+        }
+
+        void Handle_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                zxing.IsAnalyzing = false;  //読み取り停止
+
+                await DisplayAlert("通知", "次の値を読み取りました：" , "OK");
+                zxing.IsAnalyzing = true;   //読み取り再開
+            });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+            base.OnDisappearing();
         }
     }
 }
