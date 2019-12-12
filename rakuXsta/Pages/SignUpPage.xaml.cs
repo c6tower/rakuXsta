76b7a8e378ef59d6f65db8f94b70dbf7071263e2
Akira-62 <yabasu6@gmail.com>
2019-12-12 14:48:41 +0900

    add signupPage

--- rakuXsta/Pages/SignUpPage.xaml.cs
+++ rakuXsta/Pages/SignUpPage.xaml.cs
@@ -10,38 +10,32 @@ using Xamarin.Forms.Xaml;
 namespace rakuXsta.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class LoginPage : ContentPage
+    public partial class SignUpPage : ContentPage
     {
-        public LoginPage()
+        public SignUpPage()
         {
             InitializeComponent();
             var usernameEntry = new Entry { Placeholder = "Username" };
-            var passwordEntry = new Entry { Placeholder = "Password" , IsPassword = true};
-            var loginButton = new Button { Text = "Login", TextColor = Color.White, BackgroundColor = Color.FromHex("77D065") };
-            var signupButton = new Button { Text = "Sign Up" };
-
-            loginButton.Clicked += async (sender, e) =>
-            {
-                // usernameEntry.Text
-            };
+            var passwordEntry = new Entry { Placeholder = "Password", IsPassword = true };
+            var signupButton = new Button { Text = "Sign Up", TextColor = Color.White, BackgroundColor = Color.FromHex("77D065") };
 
             signupButton.Clicked += async (sender, e) =>
             {
-                 new NavigationPage(new Pages.SignUpPage());
+                new NavigationPage(new Pages.MainPage());
             };
 
 
 
             Content = new StackLayout
             {
-                Spacing = 20, Padding = 50,
+                Spacing = 20,
+                Padding = 50,
                 VerticalOptions = LayoutOptions.Center,
                 Children =
                     {
                     new Label {Text = "welcome to rakuXsta", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.FillAndExpand},
                         usernameEntry,
                         passwordEntry,
-                        loginButton,
                         signupButton
                     }
             };
