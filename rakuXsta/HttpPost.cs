4436844254f5732a14f60a8cf4dccb46d0f9d684
HoriSho <shohori13@gmail.com>
2019-12-12 15:36:59 +0900

    add HttpPostGetCreatedCardsList class

--- rakuXsta/HttpPost.cs
+++ rakuXsta/HttpPost.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
-using System.Linq;
 using System.IO;
 using System.Net;
-using System.Collections;
+using System.Text;
 using System.Web;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json;
+using System.Linq;
 
 
 namespace rakuXsta
@@ -265,4 +265,60 @@ namespace rakuXsta
         public string fk_card_id { get; set; }
         public string point { get; set; }
     }
+
+    /*
+     * 作成カード取得
+     */
+     /*
+    class HttpPostGetCreatedCardsList
+    {
+        private string token;
+        public HttpPostGetCreatedCardsList(string token)//コンストラクタ
+        {
+            this.token = token;
+        }
+
+        public CreatedCardsList<CreateItem> Exe()//実行メソッド
+        {
+            //文字コード指定
+            Encoding enc = Encoding.GetEncoding("UTF-8");
+            //POSTする文字列の指定
+            string param = "";
+            Hashtable ht = new Hashtable();
+            ht["token"] = HttpUtility.UrlEncode(token, enc);
+            foreach (string k in ht.Keys)
+            {
+                param += String.Format("{0}={1}&", k, ht[k]);
+            }
+            //バイト型配列に変換
+            byte[] data = Encoding.ASCII.GetBytes(param);
+            //WebRequestの作成
+            WebRequest req = WebRequest.Create("https://stamp-app-api.herokuapp.com/api/works");
+            //メソッドにPOSTを指定
+            req.Method = "POST";
+            //ContentTypeを"application/x-www-form-urlencoded"にする
+            req.ContentType = "application/x-www-form-urlencoded";
+            //POST送信するデータの長さを指定
+            req.ContentLength = data.Length;
+            //データをPOST送信するためのStreamを取得
+            Stream reqStream = req.GetRequestStream();
+            //送信するデータを書き込む
+            reqStream.Write(data, 0, data.Length);
+            reqStream.Close();
+            //サーバーからの応答を受信するためのWebResponseを取得
+            WebResponse res = req.GetResponse();
+            //応答データを受信するためのStreamを取得
+            Stream resStream = res.GetResponseStream();
+            //受信して表示
+            StreamReader sr = new StreamReader(resStream, enc);
+            string output = sr.ReadToEnd();
+            //閉じる
+            sr.Close();
+            //return output;
+
+            var msg = JsonConvert.DeserializeObject<LoginConfirm>(output);
+            return msg;//Login型で返してる。msg.msgでトークンが取得
+        }
+
+    }*/
 }
