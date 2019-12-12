0ee51c769b5b40d38f392b38085c3c923b0c356b
HoriSho <shohori13@gmail.com>
2019-12-12 14:32:43 +0900

    login confirm class add url changed

--- rakuXsta/HttpPost.cs
+++ rakuXsta/HttpPost.cs
@@ -41,7 +41,7 @@ namespace rakuXsta
             //バイト型配列に変換
             byte[] data = Encoding.ASCII.GetBytes(param);
             //WebRequestの作成
-            WebRequest req = WebRequest.Create("https://stamp-app-api.herokuapp.com/api/list");
+            WebRequest req = WebRequest.Create("https://stamp-app-api.herokuapp.com/api/register");
             //メソッドにPOSTを指定
             req.Method = "POST";
             //ContentTypeを"application/x-www-form-urlencoded"にする
@@ -104,7 +104,7 @@ namespace rakuXsta
             //バイト型配列に変換
             byte[] data = Encoding.ASCII.GetBytes(param);
             //WebRequestの作成
-            WebRequest req = WebRequest.Create("https://stamp-app-api.herokuapp.com/api/list");
+            WebRequest req = WebRequest.Create("https://stamp-app-api.herokuapp.com/api/authenticate");
             //メソッドにPOSTを指定
             req.Method = "POST";
             //ContentTypeを"application/x-www-form-urlencoded"にする
@@ -137,6 +137,66 @@ namespace rakuXsta
         public string token;
         public string msg;
     }
+
+    /*
+     * ログイン確認
+     */
+    class HttpPostLoginConfirm
+    {
+        private string token;
+        public HttpPostLoginConfirm(string token)//コンストラクタ
+        {
+            this.token = token;
+        }
+
+        public LoginConfirm Exe()//実行メソッド
+        {
+            //文字コード指定
+            Encoding enc = Encoding.GetEncoding("UTF-8");
+            //POSTする文字列の指定
+            string param = "";
+            Hashtable ht = new Hashtable();
+            ht["token"] = HttpUtility.UrlEncode(token, enc);
+            foreach (string k in ht.Keys)
+            {
+                param += String.Format("{0}={1}&", k, ht[k]);
+            }
+            //バイト型配列に変換
+            byte[] data = Encoding.ASCII.GetBytes(param);
+            //WebRequestの作成
+            WebRequest req = WebRequest.Create("https://stamp-app-api.herokuapp.com/api/list");
+            //メソッドにPOSTを指定
+            req.Method = "POST";
+            //ContentTypeを"application/x-www-form-urlencoded"にする
+            req.ContentType = "application/x-www-form-urlencoded";
+            //POST送信するデータの長さを指定
+            req.ContentLength = data.Length;
+            //データをPOST送信するためのStreamを取得
+            Stream reqStream = req.GetRequestStream();
+            //送信するデータを書き込む
+            reqStream.Write(data, 0, data.Length);
+            reqStream.Close();
+            //サーバーからの応答を受信するためのWebResponseを取得
+            WebResponse res = req.GetResponse();
+            //応答データを受信するためのStreamを取得
+            Stream resStream = res.GetResponseStream();
+            //受信して表示
+            StreamReader sr = new StreamReader(resStream, enc);
+            string output = sr.ReadToEnd();
+            //閉じる
+            sr.Close();
+            //return output;
+
+            var msg = JsonConvert.DeserializeObject<LoginConfirm>(output);
+            return msg;//Login型で返してる。msg.msgでトークンが取得
+        }
+
+    }
+    public class LoginConfirm
+    {
+        public string msg;
+    }
+
     class HttpPostStart
     {
         private string token;
