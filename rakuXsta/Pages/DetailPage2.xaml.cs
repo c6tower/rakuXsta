876e4a838c6e8f444cb897993b347e2ca59ab6c6
HoriSho <shohori13@gmail.com>
2019-12-18 16:10:32 +0900

    create madepage and detailpage

---
+++ rakuXsta/Pages/DetailPage2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rakuXsta.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage2 : ContentPage
+    {
+        public DetailPage2(Item items)
+        {
+            InitializeComponent();
+
+            nameValue.Text = items.Name;
+            categoryValue.Text = items.Info;
+            pointValue.Text = items.Point;
+        }
+    }
+}
--- rakuXsta/Pages/MadePage.xaml.cs
+++ rakuXsta/Pages/MadePage.xaml.cs
@@ -10,6 +10,18 @@ namespace rakuXsta.Pages
         public MadePage()
         {
             InitializeComponent();
+            //起動時所持カード読み取り処理(仮)
+            string token = "eyJhbGciOiJIUzI1NiJ9.bWlob21pZG8.T3GHHpZVlaDNiiF9RglE39Mo5U7O55OUbtu5CqN2XUg";
+            HttpPostGetCardsList obj = new HttpPostGetCardsList(token);
+            List<Item> items = obj.Exe();
+
+            // ListViewにデータソースをセット
+            cardList.ItemsSource = items;
+            //押したときのデータ
+            cardList.ItemSelected += (sender, e) =>
+            {
+                Navigation.PushAsync(new DetailPage2((Item)e.SelectedItem));
+            };
         }
     }
 }
