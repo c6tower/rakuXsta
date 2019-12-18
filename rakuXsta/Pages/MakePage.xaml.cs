fb3d53e2df5ac7b28d35252b88f0dc0a777504c9
HoriSho <shohori13@gmail.com>
2019-12-18 15:40:48 +0900

    edit makepage

--- rakuXsta/Pages/MakePage.xaml.cs
+++ rakuXsta/Pages/MakePage.xaml.cs
@@ -11,12 +11,17 @@ namespace rakuXsta.Pages
         {
             InitializeComponent();
 
+        }
+
+        public void PostCreateCardData(object sender, EventArgs e)
+        {
             string name = card_name.Text;
             string info = card_info.Text;
             string img = card_img.Text;
             string token = "eyJhbGciOiJIUzI1NiJ9.bWlob21pZG8.T3GHHpZVlaDNiiF9RglE39Mo5U7O55OUbtu5CqN2XUg";
 
             HttpPostCreateCard obj = new HttpPostCreateCard(name, info, img, token);
+            var card_data = obj.Exe();
         }
     }
 }
