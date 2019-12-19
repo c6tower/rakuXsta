82a050a016f23f01bf635c0857ea6c0894b1f6e1
HoriSho <shohori13@gmail.com>
2019-12-19 14:24:51 +0900

    appendnewdata delete

--- rakuXsta/Pages/HomePage.xaml.cs
+++ rakuXsta/Pages/HomePage.xaml.cs
@@ -9,10 +9,12 @@ namespace rakuXsta.Pages
     public partial class HomePage : ContentPage
     {
         private List<Item> items;
-        private ObservableCollection<Card> CardListData = new ObservableCollection<Card>();
         public HomePage()
         {
             InitializeComponent();
+            string token = "eyJhbGciOiJIUzI1NiJ9.bWlob21pZG8.T3GHHpZVlaDNiiF9RglE39Mo5U7O55OUbtu5CqN2XUg";
+            HttpPostGetCardsList obj = new HttpPostGetCardsList(token);
+            items = obj.Exe();
 
             // ListViewにデータソースをセット
             cardList.ItemsSource = items;
@@ -23,19 +25,15 @@ namespace rakuXsta.Pages
             };
         }
 
-        public void AppendNewData(object sender, EventArgs e)
-        {
-            CardListData.Add(new Card("追加スタンプ", "追加店", "追加ポイント"));
-            CardListData.Add(new Card("追加２スタンプ", "追加２店", "追加２ポイント"));
-        }
 
         //起動時にカードデータ取得
+        /*
         private void HomePage_Appearing(object sender, EventArgs e)
         {
             string token = "eyJhbGciOiJIUzI1NiJ9.bWlob21pZG8.T3GHHpZVlaDNiiF9RglE39Mo5U7O55OUbtu5CqN2XUg";
             HttpPostGetCardsList obj = new HttpPostGetCardsList(token);
             items = obj.Exe();
-        }
+        }*/
 
         private async void cardList_Refreshing(object sender, EventArgs e)
         {
