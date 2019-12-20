48c67482483fe8818fcd37211fb89087133c36f3
HoriSho <shohori13@gmail.com>
2019-12-20 23:56:47 +0900

    add commentout

--- rakuXsta/App.xaml.cs
+++ rakuXsta/App.xaml.cs
@@ -22,16 +22,17 @@ namespace rakuXsta
                 //インターフェイスを利用してトークンを保存する
                 ITokenInfo tokenInfo = DependencyService.Get<ITokenInfo>(DependencyFetchTarget.GlobalInstance);
                 //Akavacheで読み出し
-                /*
-                 * try catch うまくいかない、tryではmainpageに飛ぶけど、catchではloginpageに飛ばない
+                
+                // try catch うまくいかない、tryではmainpageに飛ぶけど、catchではloginpageに飛ばない
                 var loaded = await BlobCache.LocalMachine.GetObject<Token>("cache");
                 tokenInfo.TOKEN = loaded.CachedToken;
-                */
-                tokenInfo.TOKEN = "eyJhbGciOiJIUzI1NiJ9.bWlob21pZG8.T3GHHpZVlaDNiiF9RglE39Mo5U7O55OUbtu5CqN2XUg";
+                
+                //tokenInfo.TOKEN = "eyJhbGciOiJIUzI1NiJ9.bWlob21pZG8.T3GHHpZVlaDNiiF9RglE39Mo5U7O55OUbtu5CqN2XUg";
                 MainPage = new NavigationPage(new Pages.MainPage());
             }
             catch
             {
+                //飛ばない
                 MainPage = new NavigationPage(new Pages.LoginPage());
             }
         }
